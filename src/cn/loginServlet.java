f5ade41efbafef91f52c1b4caed4ed9f13b470b3
helwod151121 <1345208083@qq.com>
2015-12-07 09:19:16 +0800

    使用Servlet3.0新特性   不在web.xml中注解Servlet

--- src/cn/loginServlet.java
+++ src/cn/loginServlet.java
@@ -11,33 +11,67 @@ import java.sql.ResultSet;
 /**
  * Created by doctli on 2015/12/5.
  */
+@WebServlet(urlPatterns = {"/loginServlet"},name = "loginServlet")
 public class loginServlet extends HttpServlet {
+    String username = null;
+    String sql;
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
     }
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = null;
-        String sql;
         if (request.getParameter("userno") != (null)) {
-            sql = "select * from testmanage.student where student_no='" + request.getParameter("userno") + "';";
-            try {
-                ResultSet rs = (new DB().search(sql));
-                while (rs.next()) {
-                    username = rs.getString("student_name");
-                }
-                if (!username.equals(null)) {
-                    System.err.println("username!=null1  " + username);
-                    request.getSession().setAttribute("username", username);
-                    System.err.println("username!=null2  " + username);
-                   response.sendRedirect("student.jsp");
+            String identity=request.getParameter("identity");
+            switch (identity){
+                case "teacher":
+                    teacherlogin(request,response);
+                    break;
+                case "student":
+                    studentlogin(request,response);
+                    break;
+                default:
+                    break;
+            }
 
-                    System.err.println("username!=null3  " + username);
-                } else {
-                    System.err.println("username.equals(null)" + username);
-                }
-            } catch (Exception e) {
+        }
+    }
+    private void studentlogin(HttpServletRequest request, HttpServletResponse response){
+        sql = "select * from testmanage.student where student_no='" + request.getParameter("userno") + "';";
+        try {
+            ResultSet rs = (new DB().search(sql));
+            while (rs.next()) {
+                username = rs.getString("student_name");
+            }
+            if (!username.equals(null)) {
+                request.getSession().setAttribute("userno", request.getParameter("userno"));
+                request.getSession().setAttribute("username", username);
+                response.sendRedirect("student.jsp");
+            } else {
+                System.err.println("username.equals(null)" + username);
+            }
+        } catch (Exception e) {
+        }
+    }
+    private void teacherlogin(HttpServletRequest request, HttpServletResponse response){
+        if(request.getParameter("userno").equals("teacher01")&&request.getParameter("password").equals("123")){
+            request.getSession().setAttribute("username", "教师");
+            try {
+                response.sendRedirect("teacher.jsp");
+            }
+            catch (Exception e){
+
+            }
+
+        }
+        else {
+
+            try {
+                response.sendRedirect("error.jsp");
+            }
+            catch (Exception e){
+
             }
         }
+
     }
 }
